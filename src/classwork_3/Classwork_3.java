fe3a82a0af341dd640ccfd9bbf7161f3d3f3bdca
Aziz-Baksaliyev <nataoneorucova@gmail.com>
2026-01-17 18:01:00 +0400

    Create Classwork_3.java

---
+++ src/classwork_3/Classwork_3.java
@@ -0,0 +1,20 @@
+package classwork_3;
+
+import java.util.Arrays;
+
+public class Classwork_3 {
+	public static void main(String[] args) {
+//	1)	int[] arr = {10, 20, 30, 40, 50};
+//		System.out.println(Arrays.toString(arr));
+		
+//	2)	int[] numbers = {3, 8, 15, 22, 7, 10, 4, 9};
+//	    for (int a = 0; a < numbers.length; a++) {
+//	        if (numbers[a] % 2 == 0) {
+//	            System.out.print(numbers[a] + " ");
+//	        }
+//		
+//	    }
+//	    
+		}
+	
+}
