436aafa450c4f035036501089c38e536a7ffac78
Aziz-Baksaliyev <nataoneorucova@gmail.com>
2026-01-23 20:13:38 +0400

    Create Homework_6.java

---
+++ src/homework_6/Homework_6.java
@@ -0,0 +1,30 @@
+package homework_6;
+
+//1)
+//class Student {
+//    String name;
+//    int age;
+//    double averageGrade;
+//
+//    public Student(String name, int age, int averageGrade) {
+//        this.name = name;
+//        this.age = age;
+//        this.averageGrade = averageGrade;
+//    }
+//
+//    public void printInfo() {
+//        System.out.println("Имя: " + name + ", Возраст: " + age + ", Средний балл: " + averageGrade);
+//    }
+//}
+//
+//public class Homework_6 {
+//    public static void main(String[] args) {
+//        Student s1 = new Student("Ziya", 10, 5);
+//        Student s2 = new Student("Aziz", 13, 5);
+//        s1.printInfo();
+//        s2.printInfo();
+//    }
+//}
+//2) ..
+//3) ..
+//4) ..
