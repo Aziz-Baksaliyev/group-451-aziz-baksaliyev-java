0e4a40298e140fa74705eeaa725f44e637bec976
Aziz-Baksaliyev <nataoneorucova@gmail.com>
2026-02-12 20:12:13 +0400

    classwork_3

---
+++ src/classwork_3/Ifelse.java
@@ -0,0 +1,121 @@
+package classwork_3;
+
+public class Ifelse {
+
+	public static void main (String[] args) {
+//1)
+//		int hola = 3;
+//		if (hola%2 == 0) {
+//			System.out.print("Четное");
+//		}
+//		else {
+//			System.out.print("Нечетное");
+//		}
+//2)
+//		int hello = 1;
+//		if (hello > 0) {
+//			System.out.print("Положительное");
+//		}
+//		else if (hello == 0) {
+//			System.out.print("Ноль");
+//		}
+//		else if (hello < 0) {
+//			System.out.print("Отрицательное");
+//		}
+//3)
+//		for (int i = 1; i <= 10; i++) {
+//		    System.out.println("5 x " + i + " = " + (5 * i));
+//		}
+//4)
+//		int sum = 0;
+//		for (int i = 1; i <= 100; i++) {
+//		    sum += i;
+//		}
+//		System.out.println("Сумма: " + sum);
+//5)
+//		int target = 7;
+//		int guess = 5;
+//
+//		if (guess < target) {
+//		    System.out.println("Больше");
+//		} else if (guess > target) {
+//		    System.out.println("Меньше");
+//		} else {
+//		    System.out.println("Угадал!");
+//		}
+//6)
+//		int[] nums = {2, 5, 8, 10, 13, 15, 20, 22, 25, 30};
+//		int count = 0;
+//
+//		for (int i = 0; i < nums.length; i++) {
+//		    if (nums[i] % 2 == 0) {
+//		        count++;
+//		    }
+//		}
+//
+//		System.out.println(count);
+
+//
+//		System.out.println("Количество чётных чисел в списке: " + count);
+//7)
+//		int n = 5;
+//		long a = 1;
+//
+//		for (int i = 1; i <= n; i++) {
+//		    a *= i;
+//		}
+//		System.out.println(a);
+		
+//8)
+//		int[] numbers = {12, 45, 7, 89, 23};
+//		int max = numbers[0];
+//
+//		for (int i = 1; i < numbers.length; i++) {
+//		    if (numbers[i] > max) {
+//		        max = numbers[i];
+//		    }
+//		}
+//		System.out.println("Максимум из списка: " + max);
+//9)
+//		int height = 3;
+//		int width = 10;
+//
+//		for (int i = 0; i < height; i++) {
+//		    for (int j = 0; j < width; j++) {
+//		        System.out.print("*");
+//		    }
+//		    System.out.println();
+//		}
+//10)
+//		int rows = 5;
+//		for (int i = 1; i <= rows; i++) {
+//		    for (int j = 1; j <= i; j++) {
+//		        System.out.print("*");
+//		    }
+//		    System.out.println();
+//		}
+//11)
+//		for (int i = 10; i >= 1; i--) {
+//		    System.out.print(i + " ");
+//		}
+//12)
+//		int sum = 0;
+//
+//		for (int i = 1; i <= 50; i++) {
+//		    if (i % 2 != 0) {
+//		        sum += i;
+//		    }
+//		}
+//
+//		System.out.println(sum);
+//13)
+//		for (int i = 1; i <= 10; i++) {
+//		    for (int j = 1; j <= 10; j++) {
+//		        System.out.print(i * j + " ");
+//		    }
+//		    System.out.println();
+//		}
+//14)
+		//...
+	}
+}
