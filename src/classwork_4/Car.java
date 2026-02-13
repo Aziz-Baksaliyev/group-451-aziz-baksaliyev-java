b798744269fcd2eb0a680e2d96e1c20a0d9fbfbb
Aziz-Baksaliyev <nataoneorucova@gmail.com>
2026-02-13 20:53:22 +0400

    Create Car.java

---
+++ src/classwork_4/Car.java
@@ -0,0 +1,33 @@
+package classwork_4;
+
+public class Car {
+    String brand;
+    int speed;
+    int fuel;
+
+    public Car(String brand, int speed, int fuel) {
+        this.brand = brand;
+        this.speed = speed;
+        this.fuel = fuel;
+    }
+
+    public void drive() {
+        if (fuel > 0) {
+            fuel -= 10;
+            if (fuel < 0) fuel = 0;
+            System.out.println(brand + " проехала дистанцию. Скорость: " + speed + " км/ч. Топливо: " + fuel + "%");
+        } else {
+            System.out.println("⚠️ " + brand + " не может ехать: бак пуст!");
+        }
+    }
+
+    public void refuel(int amount) {
+        fuel += amount;
+        if (fuel > 100) fuel = 100;
+        System.out.println(brand + " заправлена на " + amount + "% Текущее топливо: " + fuel + "%");
+    }
+
+    public void showInfo() {
+        System.out.println("Машина: " + brand + " | Макс. скорость: " + speed + " | Топливо: " + fuel);
+    }
+}
