cb40c1289166945e06d959294542f294f6a7bc29
Aziz-Baksaliyev <nataoneorucova@gmail.com>
2026-03-06 21:58:45 +0400

    main.java

---
+++ src/classwork_6/Main.java
@@ -0,0 +1,66 @@
+package classwork_6;
+
+public class Main {
+//	public static int sum(int a, int b) {
+//		return a + b;
+//	}
+//
+//public static void main(String[] args) {
+//	System.out.println(sum(2, 8));
+//	}
+//}
+//
+
+
+//public static boolean isEven(int number) {
+//
+//return number % 2 == 0;
+//	}
+//
+//	for (int i = 1; i <= 10; i++) {
+//
+//	System.out.println(i);
+//	}
+//}
+//	class Person {
+//
+//		private String name;
+//		private int age;
+//		public Person(String name, int age) { 
+//			this.name = name; this.age = age; 
+//			}
+//		public String getName() { return name; }
+//		public int getAge() {
+//			return age; 
+//			}
+//		public void introduce() {
+//		System.out.println("Привет, меня зовут " + name);
+//
+//		}
+//
+//		}
+//	class Student extends Person {
+//
+//		private String major;
+//
+//		public Student(String name, int age, String major) {
+//
+//		super(name, age);
+//
+//		this.major = major;
+//
+//		}
+//
+//		@Override
+//
+//		public void introduce() {
+//
+//		super.introduce();
+//
+//		System.out.println("Я учусь на " + major);
+//
+//		}
+//
+//		}
+	
+}
